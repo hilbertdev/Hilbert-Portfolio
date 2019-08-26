d7b6673622d7608989d9c648e2d7642005333090
hilbertdev <hilbertdev@gmail.com>
2019-08-26 17:54:25 +0200

    adding css and logos

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const btnHamburger = document.querySelector('.hamburger');
+const btnHamburger = document.getElementById('navButton');
 const btnClose = document.querySelector('.closeHamburger');
+const navigation = document.getElementById('navbar');
+var isClicked = true; 
 
 btnHamburger.addEventListener('click', ()=>{
-     
+    if(isClicked){
+        navigation.style.display = 'block';   
+        isClicked = false;
+    }
+    else {
+        navigation.style.display = 'none';
+        isClicked = true;
+    }
 });
