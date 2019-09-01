89ce787567b742e3cabec5757367decb79797ea7
hilbertdev <hilbertdev@gmail.com>
2019-09-01 13:28:12 +0200

    responsive website

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const btnHamburger = document.getElementById('navButton');
 const btnClose = document.querySelector('.closeHamburger');
 const navigation = document.getElementById('navbar');
+const scroll = document.getElementById('cog');
 var isClicked = true; 
 
-btnHamburger.addEventListener('click', ()=>{
+scroll.addEventListener('click', ()=>{
     if(isClicked){
-        navigation.style.display = 'block';   
+        scroll.classList.add('fa-spin');
+        navigation.style.display = 'contents'; 
         isClicked = false;
     }
     else {
         navigation.style.display = 'none';
+        scroll.classList.remove('fa-spin');
         isClicked = true;
     }
 });
